f59c26e9fb154962acdbce08db426b3aef72e0c6
Gabor <gw@rockbox.de>
2022-05-12 11:12:20 +0100

    fixed gps pause

--- simmodule.py
+++ simmodule.py
@@ -12,8 +12,8 @@ sim_serial.flushInput()
 
 sensordata = {}
 phone_number = "015203593625"
-pause_gps = False
 rec_buff = ""
+
 
 class SIMMODULE(threading.Thread):
     def __init__(self, parent=None):
@@ -37,7 +37,10 @@ class SIMMODULE(threading.Thread):
         if sim_serial.inWaiting():
             time.sleep(0.01 )
             rec_buff = sim_serial.read(sim_serial.inWaiting())
-        if rec_buff != '':
+        if rec_buff != "":
+            #print("*******************************")
+            #print(rec_buff.decode())
+            #print("*******************************")
             if back not in rec_buff.decode():
                 #print(command + ' ERROR')
                 #print(command + ' back:\t' + rec_buff.decode())
@@ -76,6 +79,8 @@ class SIMMODULE(threading.Thread):
         #time.sleep(1)
 
     def update_cloud(self, sensordata):
+        global gps_active
+        gps_active = False
         print('')
         print('********************************')
         print(' Starting Cloud Update via SIM')
@@ -84,8 +89,8 @@ class SIMMODULE(threading.Thread):
         print('')
         print('')
 
-        if sim_serial.isOpen() == False:
-            sim_serial.open()
+        sim_serial.flushInput()
+        time.sleep(1)
         self.send_at('AT+CSQ','OK',1)
         self.send_at('AT+CGSOCKCONT=1,"IP","CMNET"','OK',1)
         self.send_at('AT+HTTPINIT','OK',1)
@@ -127,6 +132,7 @@ class SIMMODULE(threading.Thread):
         self.send_at('AT+HTTPINIT','OK',1)
         self.send_at('AT+HTTPPARA="URL","https://api.thingspeak.com/update?api_key=5BES7ZJMPH9KM58J&field7=' + str(sensordata["long"]) + '"','OK',1)
         self.send_at('AT+HTTPACTION=0 ','OK',1)
+        self.send_at('AT+HTTPTERM','OK',1)
         print('********************************')
         print('   SIM Cloud Update finished')
         print("   " + str(datetime.datetime.now()))
@@ -134,11 +140,13 @@ class SIMMODULE(threading.Thread):
         print('')
         print('')
         time.sleep(1)
+        gps_active = True
 
 
 
 
     def run(self):
+        global gps_active
         print('')
         print('********************************')
         print('      Starting SIM Module')
@@ -153,52 +161,60 @@ class SIMMODULE(threading.Thread):
 
         if sim_serial.isOpen() == False:
             sim_serial.open()
-        rec_null = True
+        gps_active = True
         answer = 0
         self.send_at('AT+CGPS=1,1','OK',1)
         time.sleep(2)
-        while rec_null:
-            answer = self.send_at('AT+CGPSINFO','+CGPSINFO: ',1)
-            if 1 == answer:
-                answer = 0
-                if ',,,,,,' in str(rec_buff):
-                    print('No GPS Fix yet')
-                    time.sleep(1)
-                else:
+        while True:
+            if gps_active:
+                answer = self.send_at('AT+CGPSINFO','+CGPSINFO: ',1)
+                #print("gps poll...")
+                if 1 == answer:
+                    answer = 0
+                    if ',,,,,,' in str(rec_buff):
+                        print('No GPS Fix yet')
+                        sensordata["alt"] = 0
+                        sensordata["speed"] = 0
+                        sensordata["lat"] = 0
+                        sensordata["long"] = 0
+                        time.sleep(1)
+                    else:
 
-                    last_position = rec_buff.decode()
-                    gps_list = last_position.split(",")
-                    try:
-                        lat = float(last_position[25:36])#/100
-                        long = float(last_position[39:51])#/100
+                        last_position = rec_buff.decode()
+                        gps_list = last_position.split(",")
+                        try:
+                            lat = float(last_position[25:36])#/100
+                            long = float(last_position[39:51])#/100
 
-                        sensordata["alt"] = gps_list[6]
-                        sensordata["speed"] = float(gps_list[7])*1.852
+                            sensordata["alt"] = gps_list[6]
+                            sensordata["speed"] = float(gps_list[7])*1.852
 
-                        #So as latitude is in format DDMM.MMMMM
-                        DDLat = int(float(lat)/100)
-                        MMLat = float(lat) - DDLat * 100
-                        sensordata["lat"] = DDLat + MMLat/60
+                            #So as latitude is in format DDMM.MMMMM
+                            DDLat = int(float(lat)/100)
+                            MMLat = float(lat) - DDLat * 100
+                            sensordata["lat"] = DDLat + MMLat/60
 
-                        DDLong = int(float(long)/100)
-                        MMLong = float(long) - DDLong * 100
-                        sensordata["long"] = DDLong + MMLong/60
-                        #print('')
-                        #print('********************************')
-                        #print('         GPS Success')
-                        #print("    lat: " + str(sensordata["lat"]))
-                        #print("   long: " + str(sensordata["long"]))
-                        #print("    alt: " + str(sensordata["alt"]))
-                        #print("  speed: " + str(sensordata["speed"]))
-                        #print('********************************')
-                        #print('')
-                        #print('')
-                    except:
-                        print('')
-                        print('********************************')
-                        print('         GPS Error')
-                        print('********************************')
-                        print('')
-                        print('')
+                            DDLong = int(float(long)/100)
+                            MMLong = float(long) - DDLong * 100
+                            sensordata["long"] = DDLong + MMLong/60
+                            #print('')
+                            #print('********************************')
+                            #print('         GPS Success')
+                            #print("    lat: " + str(sensordata["lat"]))
+                            #print("   long: " + str(sensordata["long"]))
+                            #print("    alt: " + str(sensordata["alt"]))
+                            #print("  speed: " + str(sensordata["speed"]))
+                            #print('********************************')
+                            #print('')
+                            #print('')
+                        except:
+                            sensordata["alt"] = 0
+                            sensordata["speed"] = 0
+                            print('')
+                            print('********************************')
+                            print('         GPS Error')
+                            print('********************************')
+                            print('')
+                            print('')
                     self.parent and self.parent.on_gps_data(self, sensordata)
-            #time.sleep(1.5)
+                #time.sleep(1)
